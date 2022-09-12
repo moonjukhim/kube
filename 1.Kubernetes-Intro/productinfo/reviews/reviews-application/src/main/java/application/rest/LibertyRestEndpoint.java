fdb09f3e478295931d8b15df9681cd62eb900e11
moonjukhim <50357315+moonjukhim@users.noreply.github.com>
2022-09-12 21:57:51 +0900

    Update LibertyRestEndpoint.java

--- 1.Kubernetes-Intro/productinfo/reviews/reviews-application/src/main/java/application/rest/LibertyRestEndpoint.java
+++ 1.Kubernetes-Intro/productinfo/reviews/reviews-application/src/main/java/application/rest/LibertyRestEndpoint.java
@@ -1,18 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2017 Istio Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
 package application.rest;
 
 import javax.json.Json;
@@ -43,58 +28,24 @@ public class LibertyRestEndpoint extends Application {
     private final static String ratings_service = String.format("http://%s%s:9080/ratings", ratings_hostname, services_domain);
     private final static String pod_hostname = System.getenv("HOSTNAME");
     private final static String clustername = System.getenv("CLUSTER_NAME");
-    // HTTP headers to propagate for distributed tracing are documented at
-    // https://istio.io/docs/tasks/telemetry/distributed-tracing/overview/#trace-context-propagation
     private final static String[] headers_to_propagate = {
-        // All applications should propagate x-request-id. This header is
-        // included in access log statements and is used for consistent trace
-        // sampling and log sampling decisions in Istio.
         "x-request-id",
-
-        // Lightstep tracing header. Propagate this if you use lightstep tracing
-        // in Istio (see
-        // https://istio.io/latest/docs/tasks/observability/distributed-tracing/lightstep/)
-        // Note: this should probably be changed to use B3 or W3C TRACE_CONTEXT.
-        // Lightstep recommends using B3 or TRACE_CONTEXT and most application
-        // libraries from lightstep do not support x-ot-span-context.
         "x-ot-span-context",
-
-        // Datadog tracing header. Propagate these headers if you use Datadog
-        // tracing.
         "x-datadog-trace-id",
         "x-datadog-parent-id",
         "x-datadog-sampling-priority",
-
-        // W3C Trace Context. Compatible with OpenCensusAgent and Stackdriver Istio
-        // configurations.
         "traceparent",
         "tracestate",
-
-        // Cloud trace context. Compatible with OpenCensusAgent and Stackdriver Istio
-        // configurations.
         "x-cloud-trace-context",
-
-        // Grpc binary trace context. Compatible with OpenCensusAgent nad
-        // Stackdriver Istio configurations.
         "grpc-trace-bin",
-
-        // b3 trace headers. Compatible with Zipkin, OpenCensusAgent, and
-        // Stackdriver Istio configurations. Commented out since they are
-        // propagated by the OpenTracing tracer above.
         "x-b3-traceid",
         "x-b3-spanid",
         "x-b3-parentspanid",
         "x-b3-sampled",
         "x-b3-flags",
-
-        // SkyWalking trace headers.
         "sw8",
-
-        // Application-specific headers to forward.
         "end-user",
         "user-agent",
-
-        // Context and session specific headers
         "cookie",
         "authorization",
         "jwt",
@@ -103,8 +54,8 @@ public class LibertyRestEndpoint extends Application {
     private String getJsonResponse (String productId, int starsReviewer1, int starsReviewer2) {
     	String result = "{";
     	result += "\"id\": \"" + productId + "\",";
-        result += "\"podname\": \"" + pod_hostname + "\",";
-        result += "\"clustername\": \"" + clustername + "\",";
+      result += "\"podname\": \"" + pod_hostname + "\",";
+      result += "\"clustername\": \"" + clustername + "\",";
     	result += "\"reviews\": [";
 
     	// reviewer 1:
