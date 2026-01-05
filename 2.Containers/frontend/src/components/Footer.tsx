60602e537ac5f8c6837d6cbd53eb4fe1b10ba3d0
moonjukhim <moonju.khim@gmail.com>
2026-01-05 14:56:23 +0900

    Create Footer.tsx

---
+++ 2.Containers/frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-copyright">© 2020 Google Inc.</p>
+      <p className="footer-info">
+        This website is hosted for demo purposes only. It is not an actual bank.
+      </p>
+    </footer>
+  )
+}
