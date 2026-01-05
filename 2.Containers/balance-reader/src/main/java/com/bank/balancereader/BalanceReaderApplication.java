df8aa4f65859ca2ce2df72661c907465c8dbe59a
moonjukhim <moonju.khim@gmail.com>
2026-01-05 14:54:15 +0900

    Create BalanceReaderApplication.java

---
+++ 2.Containers/balance-reader/src/main/java/com/bank/balancereader/BalanceReaderApplication.java
@@ -0,0 +1,12 @@
+package com.bank.balancereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceReaderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BalanceReaderApplication.class, args);
+    }
+}
+
