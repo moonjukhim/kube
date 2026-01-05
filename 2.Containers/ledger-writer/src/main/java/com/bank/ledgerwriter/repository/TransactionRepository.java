6ef60e2f37ac7aba7444d9b41b01ff9504092679
moonjukhim <moonju.khim@gmail.com>
2026-01-05 14:56:52 +0900

    Create TransactionRepository.java

---
+++ 2.Containers/ledger-writer/src/main/java/com/bank/ledgerwriter/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.bank.ledgerwriter.repository;
+
+import com.bank.ledgerwriter.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
+
